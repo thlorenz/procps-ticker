a4479849009e2b3f1a48bd548d3bfd74ff6d3c21
Thorsten Lorenz <thlorenz@gmx.de>
2014-06-12 01:05:14 +0000

    meminfo stream

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 'use strict';
 
 exports.readproctab = require('./lib/readproctab-readable');
+
+var sysinfo = exports.sysinfo = {};
+sysinfo.meminfo = require('./lib/meminfo-readable');
---
+++ test/meminfo-readable.js
@@ -0,0 +1,38 @@
+'use strict';
+/*jshint asi: true */
+
+var test = require('tap').test
+var meminfo = require('../').sysinfo.meminfo
+
+function inspect(obj, depth) {
+  console.error(require('util').inspect(obj, false, depth || 5, true));
+}
+
+var nonUnitMainBuffers;
+test('\ndefault uint', function (t) {
+  var data = [], ended;
+  var stream = meminfo( { interval: 50 })
+    .on('data', [].push.bind(data))
+    .on('end', function () { 
+      nonUnitMainBuffers = data[0].mainBuffers;
+      t.equal(data.length, 2, 'pushed to meminfos')
+      t.ok(nonUnitMainBuffers > 0, 'first meminfo has non zero mainBuffers')
+      t.end()
+    })
+
+  setTimeout(stream.endSoon.bind(stream), 140);
+})
+
+test('\ncustom unit k', function (t) {
+  var data = [], ended;
+  var stream = meminfo( { interval: 50, unit: 'k' })
+    .on('data', [].push.bind(data))
+    .on('end', function () { 
+      t.equal(data.length, 2, 'pushed to meminfos')
+      t.ok(data[0].mainBuffers > 0, 'first meminfo has non zero mainBuffers')
+      t.ok(data[0].mainBuffers < nonUnitMainBuffers, 'first meminfo mainBuffers is smaller than non-unit mainBuffers')
+      t.end()
+    })
+
+  setTimeout(stream.endSoon.bind(stream), 140);
+})
