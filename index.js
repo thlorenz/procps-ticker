8fa95183e7c5659f1bec31957b05f33dc2b3a88d
Thorsten Lorenz <thlorenz@gmx.de>
2014-06-12 01:13:55 +0000

    vminfo stream

--- index.js
+++ index.js
@@ -3,4 +3,6 @@
 exports.readproctab = require('./lib/readproctab-readable');
 
 var sysinfo = exports.sysinfo = {};
+
 sysinfo.meminfo = require('./lib/meminfo-readable');
+sysinfo.vminfo  = require('./lib/vminfo-readable');
---
+++ test/vminfo-readable.js
@@ -0,0 +1,22 @@
+'use strict';
+/*jshint asi: true */
+
+var test = require('tap').test
+var vminfo = require('../').sysinfo.vminfo
+
+function inspect(obj, depth) {
+  console.error(require('util').inspect(obj, false, depth || 5, true));
+}
+
+test('\nvminfo', function (t) {
+  var data = [], ended;
+  var stream = vminfo ( { interval: 50 })
+    .on('data', [].push.bind(data))
+    .on('end', function () { 
+      t.equal(data.length, 2, 'pushed to vminfos')
+      t.ok(data[0].pgpgout > 0, 'first vminfo has non zero pgpgout')
+      t.end()
+    })
+
+  setTimeout(stream.endSoon.bind(stream), 140);
+})
